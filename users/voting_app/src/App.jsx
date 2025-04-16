69c6bc27caac024f2650598e0487c6e1dae7400b
unknown <tsimipatrick28@gmail.com>
2025-04-16 08:44:31 -0700

    app_route

--- users/voting_app/src/App.jsx
+++ users/voting_app/src/App.jsx
@@ -1,7 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './components/Candidates/Home';
-import AdminPage from './components/Admin/AdminPage';
-import MonetbilSyncUtility from './components/Admin/MonetbilSyncUtility';
 import CandidatePage from './components/Candidates/CandidatePage';
 
 function App() {
@@ -9,8 +7,6 @@ function App() {
     <Router>   
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/admin" element={<AdminPage />} />
-        <Route path="/util" element={<MonetbilSyncUtility />} />
         <Route path="/candidate/:id" element={<CandidatePage />} />
       </Routes>
     </Router>
